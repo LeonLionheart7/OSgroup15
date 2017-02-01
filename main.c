47a5ddd66d7d47fd66b94802f659193ae1dcbd0a
TheSeeker427 <TheSeeker427@users.noreply.github.com>
2017-02-01 18:02:10 -0500

    Use queues instead

--- main.c
+++ main.c
@@ -12,12 +12,20 @@ typedef struct process
 #define true 1;
 #define false 0;
 
+int queRear = 0;
+int queIndex = 0;
+process *que;
+
 void RoundRobin(int, int, process[]);
+void deque();
 
 void main()
 {
     process p1 = {0, 3, 5, "P1"};
     process p2 = {1, 0, 9, "P2"};
+    
+    process q[2];
+    que = q;
     
     process processes[] = { p1, p2 };
     //printf("%d", processes[1].id);
@@ -34,20 +42,22 @@ void RoundRobin(int runTill, int num_process, process processes[])
     int lowestBurst =0;
     int currentProcess = 0; //current process
     int numOfProcessesLoaded = 0;
+    process queue[num_process];
     //Loop till end of runTill
     while(time <= runTill)
     {
         //loop through processes
         for(i = 0; i < num_process; i++){
+            
             //check if just arrived
             if(processes[i].arrivalTime == time)
             {
                 
                 //if there is no current process, assign this to be.
                 if(numOfProcessesLoaded == 0){
-                    currentProcess = i;
-                    lowestBurst = currentProcess;
-                    numOfProcessesLoaded++;
+                    //Que it
+                    que[queRear] = processes[i];
+                    queRear++;
                 }
                 printf("Time %d: %s arrived\n", time, processes[i].name);
                 processes[i].hasArrived = true;
@@ -57,24 +67,42 @@ void RoundRobin(int runTill, int num_process, process processes[])
         //this is the process where the processor Processes the Process
         if(quantum != 0)
         {
-            processes[currentProcess].burstTime--;
-            printf("Time %d: %s burst: %d\n", time, processes[currentProcess].name, processes[currentProcess].burstTime);
-            quantum--;
+            if(queRear != 0){
+                
+                printf("Time %d: %s burst: %d\n", time, que[0].name, que[0].burstTime);
+                que[0].burstTime--;
+                quantum--;
+            }
+            if(que[0].burstTime == 0){
+                printf("Time %d: %s Finished\n", time, que[0].name);
+            }
         }
         else{
-               
-            quantum = 2;
-            
-            //loop through processes
-            for(i = 0; i < num_process; i++)
-            {
-                if(processes[lowestBurst].burstTime < processes[i].burstTime && currentProcess != lowestBurst){
-                    lowestBurst = i;
-                }
+            if(que[0].burstTime == 0){
+                printf("Time %d: %s Finished\n", time, que[0].name);
             }
-            currentProcess = lowestBurst;
+            quantum = 2;
+            if(que[0].burstTime >= 0){
+                que[queRear] = que[0];
+                queRear++;
+            }
+            deque();
+
         }
         time++;
     }
         
+}
+
+void deque(){
+    int i = 0;
+    if(queRear == 0){
+        return;
+    }
+    while(i < queRear){
+            que[i] = que[i+1];
+            i++;
+    }
+        queRear--;
+    
 }
